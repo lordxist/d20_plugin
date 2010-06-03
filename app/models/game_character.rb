81c60df79fbcca6205312c20db4ea3ab4361c0fb
lordxist <julian.jabs@web.de>
2010-06-03 15:12:03 +0200

    refactored GameCharacter using groupable_attributes plugin

--- app/models/game_character.rb
+++ app/models/game_character.rb
@@ -1,14 +1,5 @@
 # The standard character.
 #
 class GameCharacter < ActiveRecord::Base
-  def abilities
-    {
-      :str => str,
-      :dex => dex,
-      :con => con,
-      :int => int,
-      :wis => wis,
-      :cha => cha
-    }
-  end
+  attribute_collection :abilities, [:str, :dex, :con, :int, :wis, :cha]
 end
